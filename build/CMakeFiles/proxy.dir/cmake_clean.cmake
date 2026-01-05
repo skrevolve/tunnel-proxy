fcab453636f7aa8787be46e4d597f8f0e946acfe
skrevolve <sukyu0919@naver.com>
2026-01-06 00:43:10 +0900

    init

---
+++ build/CMakeFiles/proxy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proxy.dir/src/main.cpp.o"
+  "CMakeFiles/proxy.dir/src/main.cpp.o.d"
+  "proxy"
+  "proxy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proxy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "Hello, Proxy!" << std::endl;
+    return 0;
+}
